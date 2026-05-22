90060dc87363e16333dead9f4ed5f567538f2b5d
Singhal-Parth <singhalparth07@gmail.com>
2026-05-22 09:31:06 +0000

    Seventh

--- src/DSUC/sorting/Quick_sort.c
+++ src/DSUC/sorting/Quick_sort.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 
-
-int partition(int arr[] , int low , int high)
+int partition(int arr[], int low, int high)
 {
     int pivot = arr[high];
     int i = low - 1;
-    int j , temp;
+    int j, temp;
 
-    for(j = low; j < high ; j++)
+    for(j = low; j < high; j++)
     {
         if(arr[j] <= pivot)
         {
@@ -19,28 +18,39 @@ int partition(int arr[] , int low , int high)
         }
     }
 
-    temp = arr[i+1];
-    arr[i++1] = arr[high];
+    temp = arr[i + 1];
+    arr[i + 1] = arr[high];
     arr[high] = temp;
 
-    return i+1;
+    return i + 1;
 }
 
-void QuickSort(int arr[] , int low , int high)
+void quickSort(int arr[], int low, int high)
 {
     int pivot_Index;
 
     if(low < high)
     {
-        pivot_Index = partition(arr , low , high);
+        pivot_Index = partition(arr, low, high);
 
-        quickSort(arr , low , pivot_Index - 1);
-        quickSort(arr , pivot_Index + 1 , high)
+        quickSort(arr, low, pivot_Index - 1);
+        quickSort(arr, pivot_Index + 1, high);
     }
 }
 
 int main()
 {
+    int arr[] = {10, 7, 8, 9, 1, 5};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    quickSort(arr, 0, n - 1);
+
+    printf("Sorted Array: ");
+
+    for(int i = 0; i < n; i++)
+    {
+        printf("%d ", arr[i]);
+    }
 
     return 0;
 }
--- src/DSUC/sorting/merge_sort.c
+++ src/DSUC/sorting/merge_sort.c
@@ -1,29 +1,32 @@
 #include <stdio.h>
 
+#define MAX 100
 
-void merge(int arr[] , int left , int mid, int right)
+void merge(int arr[], int left, int mid, int right)
 {
-    int i , j , k;
+    int i, j, k;
 
     int n1 = mid - left + 1;
     int n2 = right - mid;
 
-    int leftArr[MAX] , rightArr[MAX];
+    int leftArr[MAX], rightArr[MAX];
 
-    for(i = 0 ; i < n1 ; i++)
+    // Copy data to temporary arrays
+    for(i = 0; i < n1; i++)
     {
-        leftArr[i] = arr[left + i]
+        leftArr[i] = arr[left + i];
     }
 
-    for(j = 0 ; j < n2 ; j++)
+    for(j = 0; j < n2; j++)
     {
-        rightArr[j] = arr[mid + j + 1]
+        rightArr[j] = arr[mid + 1 + j];
     }
 
-    i = 0 ;
-    j = 0 ;
-    k = 0;
+    i = 0;
+    j = 0;
+    k = left;
 
+    // Merge the temporary arrays
     while(i < n1 && j < n2)
     {
         if(leftArr[i] <= rightArr[j])
@@ -31,38 +34,57 @@ void merge(int arr[] , int left , int mid, int right)
             arr[k] = leftArr[i];
             i++;
         }
-        
-        else{
+        else
+        {
             arr[k] = rightArr[j];
             j++;
         }
         k++;
     }
 
-    while(i < n1 )
+    // Copy remaining elements of leftArr[]
+    while(i < n1)
     {
         arr[k] = leftArr[i];
         i++;
+        k++;
     }
 
-    while(j < n2 )
+    // Copy remaining elements of rightArr[]
+    while(j < n2)
     {
         arr[k] = rightArr[j];
         j++;
+        k++;
     }
-
 }
 
-void mergesort(int arr[] , int left , int right){
-    int mid;
+void mergeSort(int arr[], int left, int right)
+{
+    if(left < right)
+    {
+        int mid = left + (right - left) / 2;
 
-    if(left  > right ){
-        mid = left +(right-left)/2;
-    
+        mergeSort(arr, left, mid);
+        mergeSort(arr, mid + 1, right);
 
-    mergeSort(arr , left , mid);
-    mergeSort(arr, mid + 1, right);
-    
-    merge(arr , left , mid , right);
+        merge(arr, left, mid, right);
     }
+}
+
+int main()
+{
+    int arr[] = {38, 27, 43, 3, 9, 82, 10};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    mergeSort(arr, 0, n - 1);
+
+    printf("Sorted Array: ");
+
+    for(int i = 0; i < n; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
 }
